se7enstar25/tensorflow

commit b0b763203e98ea616f44678e194470791db7188d

Add ability for functions to share rendezvous
The private `_shared_rendezvous` property allows the function to use the
rendezvous of the parent. This is only needed in order to support code
where raw send/recv operations are inserted and when functions are run
in graph mode where they may not be inlined.

PiperOrigin-RevId: 315319264
Change-Id: Ieb6b3924c51ccfd201b4693f3a499f883c7c0b71

tensorflow/python/eager/function_test.py
@@ -60,9 +60,11 @@ from tensorflow.python.ops import check_ops
 from tensorflow.python.ops import clip_ops
 from tensorflow.python.ops import control_flow_ops
 from tensorflow.python.ops import data_flow_ops
+from tensorflow.python.ops import functional_ops
 from tensorflow.python.ops import gen_functional_ops
 from tensorflow.python.ops import gen_random_ops
 from tensorflow.python.ops import gen_resource_variable_ops
+from tensorflow.python.ops import gen_sendrecv_ops
 from tensorflow.python.ops import gradients_impl
 from tensorflow.python.ops import init_ops
 from tensorflow.python.ops import list_ops
@@ -857,6 +859,60 @@ class FunctionTest(test.TestCase, parameterized.TestCase):
     # `pool.map` below instantiates 100 functions, one for each object.
     pool.map(stateful, [object() for _ in range(100)])
     self.assertEqual(float(v.read_value()), 0.0)
+
+  def testShareRendezvous(self):
+
+    # Disable grappler from inlining the functions. Note we run the send & recv
+    # in graph mode since with eager mode the function should automatically be
+    # inlined.
+    context.context().set_optimizer_experimental_options(
+        {'disable_meta_optimizer': True})
+
+    cpu = '/device:CPU:0'
+
+    signature = [tensor_spec.TensorSpec([], dtypes.int32)]
+
+    @def_function.function
+    def send():
+      x = constant_op.constant(1)
+      gen_sendrecv_ops.send(x, 'x', cpu, 0, cpu)
+      return x
+
+    send._shared_rendezvous = True  # pylint: disable=protected-access
+
+    @def_function.function(input_signature=signature)
+    def send_body(n):
+      send()
+      return n - 1
+
+    @def_function.function
+    def recv():
+      return gen_sendrecv_ops.recv(dtypes.int32, 'x', cpu, 0, cpu)
+
+    recv._shared_rendezvous = True  # pylint: disable=protected-access
+
+    @def_function.function(input_signature=signature)
+    def recv_body(n):
+      recv()
+      return n - 1
+
+    @def_function.function(input_signature=signature)
+    def cond(n):
+      return n > 0
+
+    # Instead of calling the send & recv functions directly we want to call them
+    # through a functional while to ensure the rendezvous is shared across the
+    # while boundary.
+    @def_function.function
+    def fn(n):
+      functional_ops.While([n], cond.get_concrete_function(),
+                           send_body.get_concrete_function())
+      return functional_ops.While([n], cond.get_concrete_function(),
+                                  recv_body.get_concrete_function())
+
+    # Use a graph context since functions will not be automatically inlined
+    with context.graph_mode(), self.cached_session():
+      self.evaluate(fn(2))
 
   def disabled_testRandomSeed(self):
 

tensorflow/core/framework/function.h
@@ -344,6 +344,8 @@ class FunctionLibraryDefinition : public OpRegistryInterface {
   static constexpr const char* const kDeviceRetOp = "_DeviceRetval";
   static constexpr const char* const kIntsOnDeviceAttr =
       "experimental_ints_on_device";
+  static constexpr const char* const kSharedRendezvousAttr =
+      "shared_rendezvous";
 
   static constexpr const char* const kGradientOp = "SymbolicGradient";
   static constexpr const char* const kFuncAttr = "f";

tensorflow/core/kernels/partitioned_function_ops.cc
@@ -43,7 +43,8 @@ namespace tensorflow {
 PartitionedCallOp::PartitionedCallOp(OpKernelConstruction* ctx)
     : AsyncOpKernel(ctx),
       func_(new NameAttrList),
-      config_proto_(new ConfigProto) {
+      config_proto_(new ConfigProto),
+      shared_rendezvous_(false) {
   OP_REQUIRES_OK(
       ctx, ctx->GetAttr(FunctionLibraryDefinition::kFuncAttr, func_.get()));
   string deprecated_config_serialized;
@@ -138,6 +139,11 @@ Status PartitionedCallOp::FillOutputDevices(
   if (fdef == nullptr) {
     return errors::NotFound("Failed to find definition for function \"",
                             func_->name(), "\"");
+  }
+  auto func_attrs = fdef->attr();
+  auto attr = func_attrs.find(FunctionLibraryDefinition::kSharedRendezvousAttr);
+  if (attr != func_attrs.end() && attr->second.b()) {
+    shared_rendezvous_ = true;
   }
 
   bool is_type_list;
@@ -245,6 +251,9 @@ void PartitionedCallOp::RunFunction(FunctionLibraryRuntime::Handle handle,
   run_opts.source_device =
       lib->device() == nullptr ? "" : lib->device()->name();
   run_opts.allow_dead_tensors = true;
+  if (shared_rendezvous_) {
+    run_opts.rendezvous = ctx->rendezvous();
+  }
 
   std::vector<Tensor>* rets = new std::vector<Tensor>;
   const string& func_name = func_->name();

tensorflow/core/kernels/partitioned_function_ops.h
@@ -58,6 +58,7 @@ class PartitionedCallOp : public AsyncOpKernel {
   std::unique_ptr<NameAttrList> func_;
   std::unique_ptr<ConfigProto> config_proto_;
   string executor_type_;
+  bool shared_rendezvous_;
   mutex mu_;
   // Cache the handle per FLR because this kernel may be instantiated for
   // a stateful op, different invocations of it may use different FLRs.

tensorflow/python/BUILD
@@ -3062,6 +3062,7 @@ tf_gen_op_wrapper_private_py(name = "rnn_ops_gen")
 
 tf_gen_op_wrapper_private_py(
     name = "sendrecv_ops_gen",
+    visibility = ["//tensorflow:internal"],
     deps = [
         "//tensorflow/core:sendrecv_ops_op_lib",
     ],

tensorflow/python/eager/BUILD
@@ -462,6 +462,7 @@ cuda_py_test(
         "//tensorflow/python:math_ops",
         "//tensorflow/python:random_seed",
         "//tensorflow/python:resource_variable_ops",
+        "//tensorflow/python:sendrecv_ops_gen",
         "//tensorflow/python:sparse_tensor",
         "//tensorflow/python:tensor_shape",
         "//tensorflow/python:tensor_spec",

tensorflow/python/eager/def_function.py
@@ -521,6 +521,10 @@ class Function(object):
     self._function_spec = function_lib.FunctionSpec.from_function_and_signature(
         python_function, input_signature)
     self._implements = experimental_implements
+    # If `True`, the function uses the rendezvous of the parent. This is only
+    # needed to support code where raw send/recv operations are inserted and
+    # when functions are run in graph mode where they may not be inlined.
+    self._shared_rendezvous = None
     self._autograph = autograph
     self._experimental_autograph_options = experimental_autograph_options
     self._experimental_relax_shapes = experimental_relax_shapes
@@ -629,6 +633,10 @@ class Function(object):
     if self._implements is not None:
       attributes = self._create_implements_attribute()
 
+    share = self._shared_rendezvous
+    if share is not None:
+      attributes[function_lib.SHARED_RENDEZVOUS_ATTRIBUTE_NAME] = share
+
     if self._experimental_compile is not None:
       attributes.update(_XlaMustCompile=bool(self._experimental_compile))
       if self._experimental_compile:
@@ -698,7 +706,8 @@ class Function(object):
     self._stateless_fn._name = self._name  # pylint: disable=protected-access
 
   def _clone(self, python_function):
-    return Function(
+    """Clone the function with different python function."""
+    f = Function(
         python_function=(self._python_function
                          if python_function is None else python_function),
         name=self._name,
@@ -708,6 +717,11 @@ class Function(object):
         experimental_autograph_options=self._experimental_autograph_options,
         experimental_relax_shapes=self._experimental_relax_shapes,
         experimental_compile=self._experimental_compile)
+
+    if self._shared_rendezvous:
+      f._shared_rendezvous = self._shared_rendezvous  # pylint: disable=protected-access
+
+    return f
 
   def _decorate(self, decorator):
     """Allows the captured Python function to be decorated in place.
@@ -922,8 +936,8 @@ class Function(object):
     @function_lib.defun(autograph=False)
     def initialize_variables():
       op_map = object_identity.ObjectIdentityDictionary()
-      # Stack all the var_is_initialized values into one tensor and interpret the
-      # numpy value. This will reduce the number of RPCs between client and
+      # Stack all the var_is_initialized values into one tensor and interpret
+      # the numpy value. This will reduce the number of RPCs between client and
       # worker in the remote case.
       with ops.init_scope():
         var_is_initialized = []

tensorflow/python/eager/function.py
@@ -86,6 +86,7 @@ ag_ctx = lazy_loader.LazyLoader(
 FORWARD_FUNCTION_ATTRIBUTE_NAME = "forward_function_name"
 BACKWARD_FUNCTION_ATTRIBUTE_NAME = "backward_function_name"
 IMPLEMENTS_ATTRIBUTE_NAME = "_implements"
+SHARED_RENDEZVOUS_ATTRIBUTE_NAME = "shared_rendezvous"
 
 
 def _make_input_signature_hashable(elem, variable_map=None):

commit 82ecc195cae6d202c63e97440fe78f24ff0d6a58

[ROCm] Fix for ROCm CSB breakage - 200609
The test `//tensorflow/python/eager:function_test_gpu` started failing today (200609), with the following error

```
======================================================================
FAIL: testSwapImplementationInEager (__main__.FunctionTest)
testSwapImplementationInEager (__main__.FunctionTest)
----------------------------------------------------------------------
Traceback (most recent call last):
  File "/root/.cache/bazel/_bazel_root/efb88f6336d9c4a18216fb94287b8d97/execroot/org_tensorflow/bazel-out/k8-opt/bin/tensorflow/python/eager/function_test_gpu.runfiles/org_tensorflow/tensorflow/python/eager/function_test.py", line 2824, in testSwapImplementationInEager
    self.assertEqual(run_on_cpu(constant_op.constant(1)).numpy(), 3)
AssertionError: 5 != 3

----------------------------------------------------------------------
Ran 69 tests in 24.622s

FAILED (failures=1, skipped=1)
```

This failure seems to have been introduced by the commit b0b763203e98ea616f44678e194470791db7188d

The failing subtest is `testSwapImplementationInEager`, which I think relies on the `disable_meta_optimizer` being set to `False` (which is the default value). There is another subtest `testSharedRendezvous` within this unit-test that explicitly sets the same flag to `True`. If the `testSharedRendezvous` subtest runs before the `testSwapImplementationInEager` subtest, then the flag `disable_meta_optimizer` gets set to `True` and leads to the failure shown above (in the `testSwapImplementationInEager` subtest)

The "fix" is explicitly set the `disable_meta_optimizer` flag to `False` in the `testSwapImplementationInEager` subtest

tensorflow/python/eager/function_test.py
@@ -2799,8 +2799,14 @@ class FunctionTest(test.TestCase, parameterized.TestCase):
     if not context.executing_eagerly():
       self.skipTest('eager only')
 
+    # testSharedRendezvous sets the disable_meta_optimizer flag to True
+    # if that subtest runs before this one, then having that set to True
+    # will cause this subtest to fail. To avoid that scenario, explicitly
+    # set the disable_meta_optimizer flag to false here
     context.context().set_optimizer_experimental_options(
-        {'min_graph_nodes': -1, 'implementation_selector': True})
+        {'min_graph_nodes': -1,
+         'implementation_selector': True,
+         'disable_meta_optimizer': False})
 
     @function.defun_with_attributes(
         attributes={'api_implements': 'foo',