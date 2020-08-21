se7enstar25/tensorflow

commit 67d15573a776119d5a544ed266dc2514ae13c3b5

Improve performance of float16 GPU ResizeBilinear.
Before there was no GPU kernel registered, so this change registers it. Unfortunately, the gradient is slow in float16 on the GPU, and also accumulates in float16 on both the CPU and GPU, so the gradient is run in float32 then casted to float16.

Fixes https://github.com/tensorflow/tensorflow/issues/41934. Thank you @edwardyehuang for discovering this issue.

Running the benchmark https://gist.github.com/reedwm/ed3fe4f2bf508a114e95b54bd39350f4 on a Titan V:

Before this change:
time for float32 (32, 16, 16, 16)->(500, 500): 0.010073661804199219
time for float16 (32, 16, 16, 16)->(500, 500): 5.622403621673584
time for float32 (32, 16, 16, 16)->(500, 500) grad: 0.05741095542907715
time for float16 (32, 16, 16, 16)->(500, 500) grad: 42.245511293411255

After this change:
time for float32 (32, 16, 16, 16)->(500, 500): 0.010007143020629883
time for float16 (32, 16, 16, 16)->(500, 500): 0.021726608276367188
time for float32 (32, 16, 16, 16)->(500, 500) grad: 0.05744624137878418
time for float16 (32, 16, 16, 16)->(500, 500) grad: 0.07420516014099121

PiperOrigin-RevId: 327680866
Change-Id: I57313bf0b0b7416b2548701c29d3ac6756dfa022

tensorflow/core/kernels/image/resize_bilinear_op.cc
@@ -16,6 +16,10 @@ limitations under the License.
 // See docs in ../ops/image_ops.cc
 #define EIGEN_USE_THREADS
 
+#if GOOGLE_CUDA || TENSORFLOW_USE_ROCM
+#define EIGEN_USE_GPU
+#endif  // GOOGLE_CUDA || TENSORFLOW_USE_ROCM
+
 #include "tensorflow/core/kernels/image/resize_bilinear_op.h"
 
 #ifdef __SSE4_1__
@@ -30,6 +34,7 @@ limitations under the License.
 #include "tensorflow/core/framework/tensor.h"
 #include "tensorflow/core/framework/tensor_shape.h"
 #include "tensorflow/core/framework/types.h"
+#include "tensorflow/core/kernels/cast_op.h"
 #include "tensorflow/core/lib/core/status.h"
 #include "tensorflow/core/platform/logging.h"
 #include "tensorflow/core/util/image_resizer_state.h"
@@ -355,11 +360,38 @@ class ResizeBilinearOpGrad : public OpKernel {
     if (!context->status().ok()) return;
 
     TTypes<float, 4>::ConstTensor input_grad = input.tensor<float, 4>();
-    typename TTypes<T, 4>::Tensor output_grad(st.output->tensor<T, 4>());
 
-    functor::ResizeBilinearGrad<Device, T>()(
-        context->eigen_device<Device>(), input_grad, st.height_scale,
-        st.width_scale, half_pixel_centers_, output_grad);
+    if (!std::is_same<T, Eigen::half>::value) {
+      typename TTypes<T, 4>::Tensor output_grad(st.output->tensor<T, 4>());
+      functor::ResizeBilinearGrad<Device, T>()(
+          context->eigen_device<Device>(), input_grad, st.height_scale,
+          st.width_scale, half_pixel_centers_, output_grad);
+    } else {
+      // Accumulate output to float instead of half tensor, since float
+      // accumulation is more numerically stable and GPU half implementation is
+      // slow.
+      // TODO(b/165759037): Create optimized and numerically stable half
+      // implementation
+      Tensor output_grad;
+      OP_REQUIRES_OK(context, context->allocate_temp(
+                                  DT_FLOAT, st.output->shape(), &output_grad));
+      functor::ResizeBilinearGrad<Device, float>()(
+          context->eigen_device<Device>(), input_grad, st.height_scale,
+          st.width_scale, half_pixel_centers_, output_grad.tensor<float, 4>());
+      if (std::is_same<Device, CPUDevice>::value) {
+        const Device& d = context->template eigen_device<Device>();
+        st.output->template flat<Eigen::half>().device(d) =
+            output_grad.template flat<float>().template cast<Eigen::half>();
+      } else {
+        // Use cast functor instead of directly casting Eigen tensor, as
+        // otherwise we need to instantiate the cast function in a .cu.cc file
+        const Tensor& output_grad_const = output_grad;
+        functor::CastFunctor<Device, Eigen::half, float> cast;
+        const Device& device = context->template eigen_device<Device>();
+        cast(device, st.output->template flat<Eigen::half>(),
+             output_grad_const.template flat<float>());
+      }
+    }
   }
 
  private:
@@ -479,7 +511,7 @@ TF_CALL_double(REGISTER_GRAD_KERNEL);
                               .HostMemory("size"),    \
                           ResizeBilinearOp<GPUDevice, T>);
 
-TF_CALL_GPU_NUMBER_TYPES_NO_HALF(REGISTER_KERNEL);
+TF_CALL_GPU_NUMBER_TYPES(REGISTER_KERNEL);
 
 #undef REGISTER_KERNEL
 
@@ -488,7 +520,7 @@ TF_CALL_GPU_NUMBER_TYPES_NO_HALF(REGISTER_KERNEL);
       Name("ResizeBilinearGrad").Device(DEVICE_GPU).TypeConstraint<T>("T"), \
       ResizeBilinearOpGrad<GPUDevice, T>);
 
-TF_CALL_GPU_NUMBER_TYPES_NO_HALF(REGISTER_GRAD_KERNEL);
+TF_CALL_GPU_NUMBER_TYPES(REGISTER_GRAD_KERNEL);
 
 #undef REGISTER_GRAD_KERNEL
 

tensorflow/core/kernels/image/BUILD
@@ -276,7 +276,7 @@ tf_kernel_library(
 tf_kernel_library(
     name = "resize_bilinear_op",
     prefix = "resize_bilinear_op",
-    deps = IMAGE_DEPS,
+    deps = IMAGE_DEPS + ["//tensorflow/core/kernels:cast_op"],
 )
 
 tf_kernel_library(

tensorflow/core/kernels/image/resize_bilinear_op_gpu.cu.cc
@@ -442,13 +442,17 @@ struct ResizeBilinearGrad<GPUDevice, T> {
   }
 };
 
-#define DEFINE_GPU_SPECS(T)                     \
-  template struct ResizeBilinear<GPUDevice, T>; \
+#define DEFINE_GPU_SPEC(T) template struct ResizeBilinear<GPUDevice, T>;
+
+TF_CALL_GPU_NUMBER_TYPES(DEFINE_GPU_SPEC);
+
+#define DEFINE_GRAD_GPU_SPEC(T) \
   template struct ResizeBilinearGrad<GPUDevice, T>;
 
-TF_CALL_GPU_NUMBER_TYPES_NO_HALF(DEFINE_GPU_SPECS);
+TF_CALL_GPU_NUMBER_TYPES_NO_HALF(DEFINE_GRAD_GPU_SPEC);
 
-#undef DEFINE_GPU_SPECS
+#undef DEFINE_GPU_SPEC
+#undef DEFINE_GRAD_GPU_SPEC
 
 }  // namespace functor
 }  // namespace tensorflow

tensorflow/python/ops/image_grad_test.py
@@ -184,20 +184,21 @@ class ResizeBilinearOpTest(test.TestCase):
     out_shape = [1, 2, 3, 1]
     x = np.arange(0, 24).reshape(in_shape)
 
-    with self.cached_session() as sess:
-      for dtype in [np.float16, np.float32, np.float64]:
-        input_tensor = constant_op.constant(x.astype(dtype), shape=in_shape)
-        resize_out = image_ops.resize_bilinear(input_tensor, out_shape[1:3])
-        grad = sess.run(gradients_impl.gradients(resize_out, input_tensor))[0]
-        self.assertAllEqual(in_shape, grad.shape)
-        # Not using gradient_checker.compute_gradient as I didn't work out
-        # the changes required to compensate for the lower precision of
-        # float16 when computing the numeric jacobian.
-        # Instead, we just test the theoretical jacobian.
-        self.assertAllEqual([[[[1.], [0.], [1.], [0.], [1.], [0.]], [[0.], [
-            0.
-        ], [0.], [0.], [0.], [0.]], [[1.], [0.], [1.], [0.], [1.], [0.]],
-                              [[0.], [0.], [0.], [0.], [0.], [0.]]]], grad)
+    for use_gpu in [False, True]:
+      with self.cached_session(use_gpu=use_gpu) as sess:
+        for dtype in [np.float16, np.float32, np.float64]:
+          input_tensor = constant_op.constant(x.astype(dtype), shape=in_shape)
+          resize_out = image_ops.resize_bilinear(input_tensor, out_shape[1:3])
+          grad = sess.run(gradients_impl.gradients(resize_out, input_tensor))[0]
+          self.assertAllEqual(in_shape, grad.shape)
+          # Not using gradient_checker.compute_gradient as I didn't work out
+          # the changes required to compensate for the lower precision of
+          # float16 when computing the numeric jacobian.
+          # Instead, we just test the theoretical jacobian.
+          self.assertAllEqual([[[[1.], [0.], [1.], [0.], [1.], [0.]],
+                                [[0.], [0.], [0.], [0.], [0.], [0.]],
+                                [[1.], [0.], [1.], [0.], [1.], [0.]],
+                                [[0.], [0.], [0.], [0.], [0.], [0.]]]], grad)
 
 
 class ResizeBicubicOpTest(test.TestCase):

commit 6ccd674ed86fce33a65e5256592d6c46ca4590e2

Fix Windows GPU build failure in resize_blinear_op.cc.
I broke it in 67d15573a776119d5a544ed266dc2514ae13c3b5.

Before, I called a function under a `std::is_same<Device, CPUDevice>` condition which cannot be linked if Device is a GPUDevice. I would expect the function not to be generated if Device is a GPUDevice due to dead code elimination, but apparently it still is on Windows.

PiperOrigin-RevId: 327887418
Change-Id: Ib97e1abf1680c75dc072850cc69c761e10ac3e1e

tensorflow/core/kernels/image/resize_bilinear_op.cc
@@ -286,6 +286,25 @@ void resize_image(typename TTypes<T, 4>::ConstTensor images,
   }
 }
 
+template <typename Device>
+struct CastFloatToHalf {
+  void operator()(const Device& d, typename TTypes<float>::ConstFlat input,
+                  typename TTypes<Eigen::half>::Flat output) {
+    output.device(d) = input.template cast<Eigen::half>();
+  }
+};
+
+template <>
+struct CastFloatToHalf<GPUDevice> {
+  void operator()(const GPUDevice& d, typename TTypes<float>::ConstFlat input,
+                  typename TTypes<Eigen::half>::Flat output) {
+    // Use existing cast functor instead of directly casting Eigen tensor, as
+    // otherwise we need to instantiate the cast function in a .cu.cc file
+    functor::CastFunctor<GPUDevice, Eigen::half, float> cast;
+    cast(d, output, input);
+  }
+};
+
 }  // namespace
 
 // Partial specialization of ResizeBilinear functor for a CPUDevice.
@@ -378,19 +397,10 @@ class ResizeBilinearOpGrad : public OpKernel {
       functor::ResizeBilinearGrad<Device, float>()(
           context->eigen_device<Device>(), input_grad, st.height_scale,
           st.width_scale, half_pixel_centers_, output_grad.tensor<float, 4>());
-      if (std::is_same<Device, CPUDevice>::value) {
-        const Device& d = context->template eigen_device<Device>();
-        st.output->template flat<Eigen::half>().device(d) =
-            output_grad.template flat<float>().template cast<Eigen::half>();
-      } else {
-        // Use cast functor instead of directly casting Eigen tensor, as
-        // otherwise we need to instantiate the cast function in a .cu.cc file
-        const Tensor& output_grad_const = output_grad;
-        functor::CastFunctor<Device, Eigen::half, float> cast;
-        const Device& device = context->template eigen_device<Device>();
-        cast(device, st.output->template flat<Eigen::half>(),
-             output_grad_const.template flat<float>());
-      }
+      const Tensor& output_grad_const = output_grad;
+      CastFloatToHalf<Device>{}(context->template eigen_device<Device>(),
+                                output_grad_const.template flat<float>(),
+                                st.output->template flat<Eigen::half>());
     }
   }
 