se7enstar25/tensorflow

commit 15275d3a14c77e2244ae1155f93243256f08e3ed

Always use TensorFlow's own non-RTTI TypeIndex.
This avoids problems when mixing RTTI and non-RTTI codes since the type for
tensorflow::TypeIndex changes, which changes the signatures of functions
in tensorflow::ResourceManager that have tensorflow::TypeIndex parameters.
PiperOrigin-RevId: 290800752
Change-Id: I43ef0a9143cff20c0725b37cf56e5b64badff089

tensorflow/core/framework/type_index.h
@@ -17,8 +17,8 @@ limitations under the License.
 #define TENSORFLOW_CORE_FRAMEWORK_TYPE_INDEX_H_
 
 #include <string>
+
 #if defined(__GXX_RTTI) || defined(_CPPRTTI)
-#include <typeindex>
 #include <typeinfo>
 #endif  // __GXX_RTTI
 
@@ -27,61 +27,57 @@ limitations under the License.
 namespace tensorflow {
 
 // On some platforms, we would like to avoid using RTTI in order to have smaller
-// binary sizes. The following #ifdef section provides a non-RTTI
-// replacement for std::type_index (with a minimal set of functions needed by
-// the TensorFlow framework, and more can be added if necessary).
-#if !defined(__GXX_RTTI) && !defined(_CPPRTTI)
-
-// A thin TypeIndex class that mimics std::type_index but does not use RTTI. As
-// a result, it does not provide the actual name of the type, and only returns a
-// pre-baked string specifying that RTTI is disabled.
-// The hash code provided in this class is unique for each class. However, it is
-// generated at runtime so this hash code should not be serialized - the value
-// for the same type can change from run to run.
+// binary sizes. This file provides a thin TypeIndex class that mimics
+// std::type_index but does not use RTTI (with a minimal set of functions needed
+// by the TensorFlow framework, and more can be added if necessary). In the
+// absence of RTTI, it does not provide the actual name of the type, and only
+// returns a pre-baked string specifying that RTTI is disabled. The hash code
+// provided in this class is unique for each class. However, it is generated at
+// runtime so this hash code should not be serialized - the value for the same
+// type can change from run to run.
 class TypeIndex {
  public:
-  TypeIndex(const TypeIndex& src) : hash_(src.hash_) {}
+  TypeIndex(const TypeIndex& src) : hash_(src.hash_), name_(src.name_) {}
   TypeIndex& operator=(const TypeIndex& src) {
     hash_ = src.hash_;
+    name_ = src.name_;
     return *this;
   }
   bool operator==(const TypeIndex& rhs) const { return (hash_ == rhs.hash_); }
   bool operator!=(const TypeIndex& rhs) const { return (hash_ != rhs.hash_); }
   ~TypeIndex() {}
 
-  const char* name() const { return "[RTTI disabled for Android]"; }
+  const char* name() const { return name_; }
+
   uint64 hash_code() const { return hash_; }
 
   // Returns a TypeIndex object that corresponds to a typename.
   template <typename T>
-  static TypeIndex Make() {
+  static TypeIndex Make(const char* name) {
     static bool hash_bit[1];
-    return TypeIndex(static_cast<uint64>(reinterpret_cast<intptr_t>(hash_bit)));
+    return TypeIndex(static_cast<uint64>(reinterpret_cast<intptr_t>(hash_bit)),
+                     name);
   }
 
  private:
   // We hide the constructor of the TypeIndex class. Use the templated
   // Make<T>() function to create a TypeIndex object.
-  TypeIndex(const uint64 hash) : hash_(hash) {}
+  explicit TypeIndex(const uint64 hash, const char* name)
+      : hash_(hash), name_(name) {}
   uint64 hash_;
+  const char* name_;
 };
 
 template <typename T>
 inline TypeIndex MakeTypeIndex() {
-  return TypeIndex::Make<T>();
-}
-
-#else  // __GXX_RTTI
-
-// In the presence of RTTI, we will simply delegate to std::type_index for
-// runtime type inference.
-typedef std::type_index TypeIndex;
-template <typename T>
-inline TypeIndex MakeTypeIndex() {
-  return TypeIndex(typeid(T));
-}
-
+#if defined(__GXX_RTTI) || defined(_CPPRTTI)
+  // Use the real type name if we have RTTI.
+  return TypeIndex::Make<T>(typeid(T).name());
+#else
+  return TypeIndex::Make<T>("[RTTI disabled]");
 #endif  // __GXX_RTTI
+}
+
 }  // namespace tensorflow
 
 #endif  // TENSORFLOW_CORE_FRAMEWORK_TYPE_INDEX_H_

commit 673474a5951548c3997b08e508d46feb49b8083d

This bug was introduced in 15275d3 - because TF libs got loaded the second time somewhere in its life cycle (probably dynamically when there are multiple saved_model models in the same inference environment, please see https://github.com/tensorflow/tensorflow/issues/44428 for examples and fixes), RTTI changes for every class for every new dynamic library loading, thus TensorList got different RTTI ID and decoder in variant->get<VariantTensorDataProto>() refuses to decode protobuf.
You have fixed this for MacOS by reverting to the old behaviour (and for
Windows in master branch), now my patch forces this for everyone else.

tensorflow/core/framework/type_index.h
@@ -23,10 +23,7 @@ limitations under the License.
 #endif  // __GXX_RTTI
 
 #include "tensorflow/core/platform/types.h"
-
-#if defined(MACOS) || defined(TARGET_OS_MAC) || defined(PLATFORM_WINDOWS)
 #include "tensorflow/core/platform/hash.h"
-#endif  // defined(MACOS) || defined(TARGET_OS_MAC) || defined(PLATFORM_WINDOWS)
 
 namespace tensorflow {
 
@@ -62,15 +59,9 @@ class TypeIndex {
 
 #if defined(__GXX_RTTI) || defined(_CPPRTTI)
 
-#if defined(MACOS) || defined(TARGET_OS_MAC) || defined(PLATFORM_WINDOWS)
     // Use a hash based on the type name to avoid issues due to RTLD_LOCAL on
     // MacOS (b/156979412).
     return TypeIndex(Hash64(typeid(T).name()), typeid(T).name());
-#else
-    // Use the real type name if we have RTTI.
-    return TypeIndex(static_cast<uint64>(reinterpret_cast<intptr_t>(hash_bit)),
-                     typeid(T).name());
-#endif  // defined(MACOS) || defined(TARGET_OS_MAC) || defined(PLATFORM_WINDOWS)
 
 #else
 #if TARGET_OS_OSX