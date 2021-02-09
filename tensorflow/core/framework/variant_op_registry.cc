se7enstar25/tensorflow

commit de37b1eaca05431822223e5c996bc08245cf523b

Refactor the mobile/portable/android/ios targets to use filegroups where possible.
PiperOrigin-RevId: 290126994
Change-Id: I5e456b788e306cef294882445cd2bf9005343dd5

tensorflow/core/framework/variant_op_registry.cc
@@ -32,13 +32,6 @@ std::unordered_set<string>* UnaryVariantOpRegistry::PersistentStringStorage() {
   return string_storage;
 }
 
-// static
-UnaryVariantOpRegistry* UnaryVariantOpRegistry::Global() {
-  static UnaryVariantOpRegistry* global_unary_variant_op_registry =
-      new UnaryVariantOpRegistry;
-  return global_unary_variant_op_registry;
-}
-
 UnaryVariantOpRegistry::VariantDecodeFn* UnaryVariantOpRegistry::GetDecodeFn(
     StringPiece type_name) {
   auto found = decode_fns.find(type_name);
@@ -102,28 +95,6 @@ REGISTER_VARIANT_DECODE_TYPE(double);
 
 #undef REGISTER_VARIANT_DECODE_TYPE
 
-UnaryVariantOpRegistry::AsyncVariantDeviceCopyFn*
-UnaryVariantOpRegistry::GetDeviceCopyFn(
-    const VariantDeviceCopyDirection direction, const TypeIndex& type_index) {
-  auto found = device_copy_fns.find(std::make_pair(direction, type_index));
-  if (found == device_copy_fns.end()) return nullptr;
-  return &found->second;
-}
-
-void UnaryVariantOpRegistry::RegisterDeviceCopyFn(
-    const VariantDeviceCopyDirection direction, const TypeIndex& type_index,
-    const AsyncVariantDeviceCopyFn& device_copy_fn) {
-  AsyncVariantDeviceCopyFn* existing = GetDeviceCopyFn(direction, type_index);
-  CHECK_EQ(existing, nullptr)
-      << "UnaryVariantDeviceCopy for direction: " << direction
-      << " and type_index: " << port::MaybeAbiDemangle(type_index.name())
-      << " already registered";
-  device_copy_fns.insert(
-      std::pair<std::pair<VariantDeviceCopyDirection, TypeIndex>,
-                AsyncVariantDeviceCopyFn>(std::make_pair(direction, type_index),
-                                          device_copy_fn));
-}
-
 Status VariantDeviceCopy(
     const VariantDeviceCopyDirection direction, const Variant& from,
     Variant* to,
@@ -171,26 +142,6 @@ REGISTER_VARIANT_DEVICE_COPY_TYPE(bool);
 
 #undef REGISTER_VARIANT_DEVICE_COPY_TYPE
 
-// Special casing UnaryOpFn per op and per device.
-UnaryVariantOpRegistry::VariantUnaryOpFn* UnaryVariantOpRegistry::GetUnaryOpFn(
-    VariantUnaryOp op, StringPiece device, const TypeIndex& type_index) {
-  auto found = unary_op_fns.find({op, device, type_index});
-  if (found == unary_op_fns.end()) return nullptr;
-  return &found->second;
-}
-
-void UnaryVariantOpRegistry::RegisterUnaryOpFn(
-    VariantUnaryOp op, const string& device, const TypeIndex& type_index,
-    const VariantUnaryOpFn& unary_op_fn) {
-  VariantUnaryOpFn* existing = GetUnaryOpFn(op, device, type_index);
-  CHECK_EQ(existing, nullptr)
-      << "Unary VariantUnaryOpFn for type_index: "
-      << port::MaybeAbiDemangle(type_index.name())
-      << " already registered for device type: " << device;
-  unary_op_fns.insert(std::pair<FuncTuple<VariantUnaryOp>, VariantUnaryOpFn>(
-      {op, GetPersistentStringPiece(device), type_index}, unary_op_fn));
-}
-
 namespace {
 template <typename T>
 Status ZerosLikeVariantPrimitiveType(OpKernelContext* ctx, const T& t,
@@ -212,27 +163,6 @@ REGISTER_VARIANT_ZEROS_LIKE_TYPE(double);
 REGISTER_VARIANT_ZEROS_LIKE_TYPE(bool);
 
 #undef REGISTER_VARIANT_ZEROS_LIKE_TYPE
-
-// Special casing BinaryOpFn per op and per device.
-UnaryVariantOpRegistry::VariantBinaryOpFn*
-UnaryVariantOpRegistry::GetBinaryOpFn(VariantBinaryOp op, StringPiece device,
-                                      const TypeIndex& type_index) {
-  auto found = binary_op_fns.find({op, device, type_index});
-  if (found == binary_op_fns.end()) return nullptr;
-  return &found->second;
-}
-
-void UnaryVariantOpRegistry::RegisterBinaryOpFn(
-    VariantBinaryOp op, const string& device, const TypeIndex& type_index,
-    const VariantBinaryOpFn& add_fn) {
-  VariantBinaryOpFn* existing = GetBinaryOpFn(op, device, type_index);
-  CHECK_EQ(existing, nullptr)
-      << "Unary VariantBinaryOpFn for type_index: "
-      << port::MaybeAbiDemangle(type_index.name())
-      << " already registered for device type: " << device;
-  binary_op_fns.insert(std::pair<FuncTuple<VariantBinaryOp>, VariantBinaryOpFn>(
-      {op, GetPersistentStringPiece(device), type_index}, add_fn));
-}
 
 namespace {
 template <typename T>

tensorflow/core/framework/variant_op_registry.h
@@ -102,35 +102,78 @@ class UnaryVariantOpRegistry {
   // Add a copy-to-GPU function to the registry.
   void RegisterDeviceCopyFn(const VariantDeviceCopyDirection direction,
                             const TypeIndex& type_index,
-                            const AsyncVariantDeviceCopyFn& device_copy_fn);
+                            const AsyncVariantDeviceCopyFn& device_copy_fn) {
+    AsyncVariantDeviceCopyFn* existing = GetDeviceCopyFn(direction, type_index);
+    CHECK_EQ(existing, nullptr)
+        << "UnaryVariantDeviceCopy for direction: " << direction
+        << " and type_index: " << port::MaybeAbiDemangle(type_index.name())
+        << " already registered";
+    device_copy_fns.insert(
+        std::pair<std::pair<VariantDeviceCopyDirection, TypeIndex>,
+                  AsyncVariantDeviceCopyFn>(
+            std::make_pair(direction, type_index), device_copy_fn));
+  }
 
   // Returns nullptr if no copy function was found for the given
   // TypeName and direction.
   AsyncVariantDeviceCopyFn* GetDeviceCopyFn(
-      const VariantDeviceCopyDirection direction, const TypeIndex& type_index);
+      const VariantDeviceCopyDirection direction, const TypeIndex& type_index) {
+    auto found = device_copy_fns.find(std::make_pair(direction, type_index));
+    if (found == device_copy_fns.end()) return nullptr;
+    return &found->second;
+  }
 
   // Add a unary op function to the registry.
   void RegisterUnaryOpFn(VariantUnaryOp op, const string& device,
                          const TypeIndex& type_index,
-                         const VariantUnaryOpFn& unary_op_fn);
+                         const VariantUnaryOpFn& unary_op_fn) {
+    VariantUnaryOpFn* existing = GetUnaryOpFn(op, device, type_index);
+    CHECK_EQ(existing, nullptr)
+        << "Unary VariantUnaryOpFn for type_index: "
+        << port::MaybeAbiDemangle(type_index.name())
+        << " already registered for device type: " << device;
+    unary_op_fns.insert(std::pair<FuncTuple<VariantUnaryOp>, VariantUnaryOpFn>(
+        {op, GetPersistentStringPiece(device), type_index}, unary_op_fn));
+  }
 
   // Returns nullptr if no unary op function was found for the given
   // op, device, and TypeName.
   VariantUnaryOpFn* GetUnaryOpFn(VariantUnaryOp op, StringPiece device,
-                                 const TypeIndex& type_index);
+                                 const TypeIndex& type_index) {
+    auto found = unary_op_fns.find({op, device, type_index});
+    if (found == unary_op_fns.end()) return nullptr;
+    return &found->second;
+  }
 
   // Add a binary op function to the registry.
   void RegisterBinaryOpFn(VariantBinaryOp op, const string& device,
                           const TypeIndex& type_index,
-                          const VariantBinaryOpFn& add_fn);
+                          const VariantBinaryOpFn& add_fn) {
+    VariantBinaryOpFn* existing = GetBinaryOpFn(op, device, type_index);
+    CHECK_EQ(existing, nullptr)
+        << "Unary VariantBinaryOpFn for type_index: "
+        << port::MaybeAbiDemangle(type_index.name())
+        << " already registered for device type: " << device;
+    binary_op_fns.insert(
+        std::pair<FuncTuple<VariantBinaryOp>, VariantBinaryOpFn>(
+            {op, GetPersistentStringPiece(device), type_index}, add_fn));
+  }
 
   // Returns nullptr if no binary op function was found for the given
   // op, device and TypeName.
   VariantBinaryOpFn* GetBinaryOpFn(VariantBinaryOp op, StringPiece device,
-                                   const TypeIndex& type_index);
+                                   const TypeIndex& type_index) {
+    auto found = binary_op_fns.find({op, device, type_index});
+    if (found == binary_op_fns.end()) return nullptr;
+    return &found->second;
+  }
 
   // Get a pointer to a global UnaryVariantOpRegistry object
-  static UnaryVariantOpRegistry* Global();
+  static UnaryVariantOpRegistry* Global() {
+    static UnaryVariantOpRegistry* global_unary_variant_op_registry =
+        new UnaryVariantOpRegistry;
+    return global_unary_variant_op_registry;
+  }
 
   // Get a pointer to a global persistent string storage object.
   // ISO/IEC C++ working draft N4296 clarifies that insertion into an

tensorflow/core/BUILD
@@ -79,6 +79,8 @@ load(
     "tf_cc_tests",
     "tf_copts",
     "tf_cuda_library",
+    "tf_defines_nortti_if_android",
+    "tf_defines_nortti_if_emscripten",
     "tf_features_nomodules_if_android",
     "tf_features_nomodules_if_emscripten",
     "tf_gen_op_libs",
@@ -123,6 +125,7 @@ load(
     "tf_jspb_proto_library",
     "tf_kernel_tests_linkstatic",
     "tf_lib_proto_parsing_deps",
+    "tf_portable_deps_no_runtime",
     "tf_proto_library",
     "tf_proto_library_cc",
     "tf_protos_all",
@@ -1292,80 +1295,76 @@ filegroup(
     visibility = ["//visibility:public"],
 )
 
-# Core sources for Android builds.
+# Sources required to build the TensorFlow framework without the runtime on
+# mobile platforms. This is essentially the sources required to build
+# tensorflow/core/framework:tensor without using granular targets.
 filegroup(
     name = "mobile_srcs_no_runtime",
     srcs = [
         "//tensorflow/compiler/jit:mobile_srcs_no_runtime",
         "//tensorflow/core/framework:attr_value_proto_text_srcs",
         "//tensorflow/core/framework:mobile_srcs_no_runtime",
-        "//tensorflow/core/lib/bfloat16:bfloat16.cc",
-        "//tensorflow/core/lib/bfloat16:bfloat16.h",
-        "//tensorflow/core/lib/core:legacy_lib_core_all_headers",
-        "//tensorflow/core/lib/core:legacy_lib_core_all_srcs",
-        "//tensorflow/core/lib/gtl:legacy_lib_gtl_all_headers",
-        "//tensorflow/core/lib/hash:legacy_lib_hash_all_headers",
-        "//tensorflow/core/lib/hash:legacy_lib_hash_all_srcs",
-        "//tensorflow/core/lib/histogram:legacy_lib_histogram_all_headers",
-        "//tensorflow/core/lib/histogram:legacy_lib_histogram_all_srcs",
-        "//tensorflow/core/lib/io:legacy_lib_io_all_headers",
-        "//tensorflow/core/lib/io:legacy_lib_io_all_srcs",
-        "//tensorflow/core/lib/math:math_util.h",
-        "//tensorflow/core/lib/monitoring:legacy_lib_monitoring_all_headers",
-        "//tensorflow/core/lib/monitoring:legacy_lib_monitoring_all_srcs",
-        "//tensorflow/core/lib/random:legacy_lib_random_all_headers",
-        "//tensorflow/core/lib/random:legacy_lib_random_all_srcs",
-        "//tensorflow/core/lib/strings:legacy_lib_strings_all_headers",
-        "//tensorflow/core/lib/strings:legacy_lib_strings_all_srcs",
-        "//tensorflow/core/platform:legacy_mobile_srcs",
-        "//tensorflow/core/profiler:mobile_srcs",
+        "//tensorflow/core/lib/bfloat16:mobile_srcs_no_runtime",
+        "//tensorflow/core/lib/core:mobile_srcs_no_runtime",
+        "//tensorflow/core/lib/gtl:mobile_srcs_no_runtime",
+        "//tensorflow/core/lib/hash:mobile_srcs_no_runtime",
+        "//tensorflow/core/lib/strings:mobile_srcs_no_runtime",
+        "//tensorflow/core/platform:mobile_srcs_no_runtime",
         "//tensorflow/core/public:mobile_srcs_no_runtime",
-        "//tensorflow/core/util/ctc:android_srcs",
-        "//tensorflow/core/util/sparse:mobile_srcs_no_runtime_group",
         "//tensorflow/core/util:mobile_srcs_no_runtime",
     ] + glob(
         [
             "client/**/*.cc",
-            "lib/**/*.h",
-            "lib/**/*.cc",
         ],
         exclude = [
             "**/*test.*",
             "**/*testutil*",
             "**/*testlib*",
             "**/*main.cc",
-            "debug/**/*",
-            "lib/jpeg/**/*",
-            "lib/png/**/*",
-            "lib/gif/**/*",
-            "user_ops/**/*.cu.cc",
-            "common_runtime/gpu/**/*",
-            "common_runtime/eager/*",
-            "common_runtime/gpu_device_factory.*",
         ],
-    ) + if_chromiumos(
-        ["//tensorflow/core/platform:legacy_srcs_no_runtime_google"],
-        otherwise = ["//tensorflow/core/platform:legacy_srcs_no_runtime"],
     ),
     visibility = ["//visibility:private"],
 )
 
+# Sources required to build the TensorFlow framework with runtime on
+# mobile platforms without granular targets. It is assumed that the source
+# files in tensorflow/core:mobile_srcs_no_runtime have been compiled
+# separately and are linked in as a dependency.
 filegroup(
     name = "mobile_srcs_only_runtime",
     srcs = [
+        # Sources for which we do not yet have granular targets.
         "//tensorflow/c/eager:srcs",
         "//tensorflow/c:srcs",
         "//tensorflow/core/common_runtime/eager:srcs",
         "//tensorflow/core/framework:mobile_srcs_only_runtime",
         "//tensorflow/core/kernels:android_srcs",
+        "//tensorflow/core/lib/io:mobile_srcs_only_runtime",
+        "//tensorflow/core/profiler:mobile_srcs",
+        "//tensorflow/core/public:mobile_srcs_only_runtime",
         "//tensorflow/core/util/ctc:android_srcs",
+        "//tensorflow/core/util/sparse:mobile_srcs_only_runtime",
         "//tensorflow/core/util/tensor_bundle:android_srcs",
+        "//tensorflow/core/util:mobile_srcs_only_runtime",
+
+        # Sources for which we already have granular targets.
+        "//tensorflow/core/lib/core:mobile_srcs_only_runtime",
+        "//tensorflow/core/lib/gtl:mobile_srcs_only_runtime",
+        "//tensorflow/core/lib/hash:mobile_srcs_only_runtime",
+        "//tensorflow/core/lib/histogram:mobile_srcs_only_runtime",
+        "//tensorflow/core/lib/math:mobile_srcs_only_runtime",
+        "//tensorflow/core/lib/monitoring:mobile_srcs_only_runtime",
+        "//tensorflow/core/lib/random:mobile_srcs_only_runtime",
+        "//tensorflow/core/lib/strings:mobile_srcs_only_runtime",
+        "//tensorflow/core/platform:mobile_srcs_only_runtime",
     ] + glob(
         [
-            "common_runtime/**/*.h",
             "common_runtime/**/*.cc",
-            "graph/**/*.h",
+            "common_runtime/**/*.h",
             "graph/**/*.cc",
+            "graph/**/*.h",
+            "lib/wav/*.cc",
+            "lib/wav/*.h",
         ],
         exclude = [
             "**/*test.*",
@@ -1389,6 +1388,12 @@ filegroup(
     visibility = ["//visibility:public"],
 )
 
+alias(
+    name = "android_srcs",
+    actual = ":mobile_srcs",
+    visibility = ["//visibility:public"],
+)
+
 # Native library support for Android applications.  Does not contain
 # operators, use :android_tensorflow_lib if you want full operator
 # support.
@@ -1405,51 +1410,31 @@ filegroup(
 # --host_crosstool_top=@bazel_tools//tools/cpp:toolchain
 cc_library(
     name = "android_tensorflow_lib_lite",
-    srcs = if_android([":android_srcs"]),
-    copts = tf_copts(android_optimization_level_override = None) + [
-        "-DSUPPORT_SELECTIVE_REGISTRATION",
-    ],
+    srcs = if_android([":mobilesrcs"]),
+    copts = tf_copts(android_optimization_level_override = None),
+    defines = ["SUPPORT_SELECTIVE_REGISTRATION"],
     linkopts = ["-lz"],
     tags = [
         "manual",
         "notap",
     ],
     visibility = ["//visibility:public"],
-    deps = [
-        ":mobile_additional_lib_deps",
-        ":protos_all_cc_impl",
-        "//tensorflow/core/util:stats_calculator_portable",
-        "//third_party/eigen3",
-        "@com_google_protobuf//:protobuf",
-        "@double_conversion//:double-conversion",
-        "@farmhash_archive//:farmhash",
-        "@nsync//:nsync_cpp",
-    ],
     alwayslink = 1,
 )
 
 cc_library(
     name = "android_tensorflow_lib_lite_nortti",
-    srcs = if_android([":android_srcs"]),
-    copts = tf_copts(android_optimization_level_override = None) + [
-        "-DSUPPORT_SELECTIVE_REGISTRATION",
-    ] + tf_opts_nortti_if_android(),
+    srcs = if_android([":mobile_srcs"]),
+    copts = tf_copts(android_optimization_level_override = None) + tf_opts_nortti_if_android(),
+    defines = [
+        "SUPPORT_SELECTIVE_REGISTRATION",
+    ] + tf_defines_nortti_if_android(),
     linkopts = ["-lz"],
     tags = [
         "manual",
         "notap",
     ],
     visibility = ["//visibility:public"],
-    deps = [
-        ":mobile_additional_lib_deps",
-        ":protos_all_cc_impl",
-        "//tensorflow/core/util:stats_calculator_portable",
-        "//third_party/eigen3",
-        "@com_google_protobuf//:protobuf",
-        "@double_conversion//:double-conversion",
-        "@farmhash_archive//:farmhash",
-        "@nsync//:nsync_cpp",
-    ],
     alwayslink = 1,
 )
 
@@ -1461,29 +1446,6 @@ cc_library(
         "@com_google_absl//absl/container:flat_hash_set",
         "@com_google_absl//absl/strings",
     ],
-)
-
-cc_library(
-    name = "emscripten_tensorflow_lib_lite_nortti_lite_protos_no_runtime",
-    srcs = if_emscripten([":mobile_srcs_no_runtime"]),
-    copts = ["-DSUPPORT_SELECTIVE_REGISTRATION"] + tf_opts_nortti_if_emscripten(),
-    defines = ["TENSORFLOW_LITE_PROTOS"],
-    tags = [
-        "manual",
-        "notap",
-    ],
-    visibility = ["//visibility:public"],
-    deps = [
-        ":emscripten_proto_lib_no_rtti_lite_runtime",
-        ":mobile_additional_lib_deps",
-        "//tensorflow/core/util:stats_calculator_portable",
-        "//third_party/eigen3",
-        "@double_conversion//:double-conversion",
-        "@farmhash_archive//:farmhash",
-        "@nsync//:nsync_cpp",
-        "@zlib_archive//:zlib",
-    ],
-    alwayslink = 1,
 )
 
 # Native library support for iOS applications.
@@ -1513,19 +1475,10 @@ cc_library(
 
 cc_library(
     name = "ios_tensorflow_lib_lite",
-    srcs = if_ios([":android_srcs"]),
+    srcs = if_ios([":mobile_srcs"]),
     copts = tf_copts() + ["-Os"],
     visibility = ["//visibility:public"],
-    deps = [
-        ":mobile_additional_lib_deps",
-        ":protos_all_cc_impl",
-        "//tensorflow/core/util:stats_calculator_portable",
-        "//third_party/eigen3",
-        "@com_google_protobuf//:protobuf",
-        "@double_conversion//:double-conversion",
-        "@farmhash_archive//:farmhash",
-        "@nsync//:nsync_cpp",
-    ],
+    deps = tf_portable_deps_no_runtime(),
     alwayslink = 1,
 )
 
@@ -1599,7 +1552,7 @@ filegroup(
     visibility = ["//visibility:public"],
 )
 
-# This is like android_test_srcs, minus the things that are already in android_srcs.
+# This is like android_test_srcs, minus the things that are already in mobile_srcs.
 filegroup(
     name = "android_test_srcs_no_core",
     srcs = [
@@ -4655,22 +4608,4 @@ tf_portable_proto_library(
     ],
     visibility = ["//visibility:public"],
     deps = ["@com_google_protobuf//:protobuf"],
-)
-
-alias(
-    name = "android_srcs_no_runtime",
-    actual = ":mobile_srcs_no_runtime",
-    visibility = ["//visibility:public"],
-)
-
-alias(
-    name = "android_srcs_only_runtime",
-    actual = ":mobile_srcs_only_runtime",
-    visibility = ["//visibility:public"],
-)
-
-alias(
-    name = "android_srcs",
-    actual = ":mobile_srcs",
-    visibility = ["//visibility:public"],
 )

tensorflow/core/framework/BUILD
@@ -258,11 +258,44 @@ filegroup(
         "allocator.h",
         "allocator_registry.cc",
         "allocator_registry.h",
-        "attr_value_util.cc",
-        "attr_value_util.h",
         "bfloat16.cc",
         "bfloat16.h",
         "bounds_check.h",
+        "cpu_allocator_impl.cc",
+        "log_memory.cc",
+        "log_memory.h",
+        "numeric_types.h",
+        "register_types.h",
+        "resource_handle.cc",
+        "resource_handle.h",
+        "tensor.cc",
+        "tensor.h",
+        "tensor_shape.cc",
+        "tensor_shape.h",
+        "tensor_types.h",
+        "tracking_allocator.cc",
+        "tracking_allocator.h",
+        "type_index.h",
+        "type_traits.h",
+        "typed_allocator.cc",
+        "typed_allocator.h",
+        "types.cc",
+        "types.h",
+        "variant.cc",
+        "variant.h",
+        "variant_encode_decode.h",
+        "variant_op_registry.cc",
+        "variant_op_registry.h",
+        "variant_tensor_data.cc",
+        "variant_tensor_data.h",
+    ],
+)
+
+filegroup(
+    name = "mobile_srcs_only_runtime",
+    srcs = [
+        "attr_value_util.cc",
+        "attr_value_util.h",
         "cancellation.cc",
         "cancellation.h",
         "collective.cc",
@@ -270,12 +303,11 @@ filegroup(
         "common_shape_fns.cc",
         "common_shape_fns.h",
         "control_flow.h",
-        "cpu_allocator_impl.cc",
+        "dataset.cc",
+        "dataset.h",
         "dataset_stateful_op_whitelist.h",
         "device_base.cc",
         "device_base.h",
-        "fake_input.cc",
-        "fake_input.h",
         "function.cc",
         "function.h",
         "function_handle_cache.cc",
@@ -291,8 +323,6 @@ filegroup(
         "load_library.cc",
         "local_rendezvous.cc",
         "local_rendezvous.h",
-        "log_memory.cc",
-        "log_memory.h",
         "logging.cc",
         "logging.h",
         "lookup_interface.cc",
@@ -303,15 +333,16 @@ filegroup(
         "model.h",
         "node_def_builder.cc",
         "node_def_builder.h",
+        "node_def_util.cc",
         "node_def_util.h",
         "numeric_op.h",
-        "numeric_types.h",
         "op.cc",
         "op.h",
         "op_def_builder.cc",
         "op_def_builder.h",
         "op_def_util.cc",
         "op_def_util.h",
+        "op_kernel.cc",
         "op_kernel.h",
         "op_segment.cc",
         "op_segment.h",
@@ -319,16 +350,10 @@ filegroup(
         "ops_util.h",
         "partial_tensor_shape.h",
         "queue_interface.h",
-        "reader_base.cc",
-        "reader_base.h",
         "reader_interface.h",
-        "reader_op_kernel.h",
-        "register_types.h",
         "register_types_traits.h",
         "rendezvous.cc",
         "rendezvous.h",
-        "resource_handle.cc",
-        "resource_handle.h",
         "resource_mgr.cc",
         "resource_mgr.h",
         "resource_op_kernel.h",
@@ -341,49 +366,18 @@ filegroup(
         "session_state.h",
         "shape_inference.cc",
         "shape_inference.h",
-        "shared_ptr_variant.h",
         "stats_aggregator.h",
-        "tensor.cc",
-        "tensor.h",
         "tensor_interface.h",
         "tensor_reference.h",
-        "tensor_shape.cc",
-        "tensor_shape.h",
         "tensor_slice.cc",
         "tensor_slice.h",
-        "tensor_types.h",
         "tensor_util.cc",
         "tensor_util.h",
         "thread_factory.h",
-        "tracking_allocator.cc",
-        "tracking_allocator.h",
-        "type_index.h",
-        "type_traits.h",
-        "typed_allocator.cc",
-        "typed_allocator.h",
-        "types.cc",
-        "types.h",
         "unique_tensor_references.cc",
         "unique_tensor_references.h",
-        "variant.cc",
-        "variant.h",
-        "variant_encode_decode.h",
-        "variant_op_registry.cc",
-        "variant_op_registry.h",
-        "variant_tensor_data.cc",
-        "variant_tensor_data.h",
         "versions.cc",
         "versions.h",
-    ],
-)
-
-filegroup(
-    name = "mobile_srcs_only_runtime",
-    srcs = [
-        "dataset.cc",
-        "dataset.h",
-        "node_def_util.cc",
-        "op_kernel.cc",
     ],
 )
 

tensorflow/core/lib/bfloat16/BUILD
@@ -15,6 +15,15 @@ cc_library(
     ],
 )
 
+# Export source files needed for mobile builds, which do not use granular targets.
+filegroup(
+    name = "mobile_srcs_no_runtime",
+    srcs = [
+        "bfloat16.cc",
+        "bfloat16.h",
+    ],
+)
+
 # TODO(bmzhao): Remove the following once references in core/BUILD is removed.
 exports_files(
     glob(["*"]),

tensorflow/core/lib/core/BUILD
@@ -140,6 +140,35 @@ tf_proto_library(
     exports = ["//tensorflow/core:error_codes_proto_impl"],
 )
 
+# Export source files needed for mobile builds, which do not use granular targets.
+filegroup(
+    name = "mobile_srcs_no_runtime",
+    srcs = [
+        "blocking_counter.h",
+        "coding.h",
+        "errors.h",
+        "refcount.h",
+        "status.h",
+        "stringpiece.h",
+    ],
+    visibility = ["//tensorflow/core:__pkg__"],
+)
+
+filegroup(
+    name = "mobile_srcs_only_runtime",
+    srcs = [
+        "arena.cc",
+        "arena.h",
+        "bitmap.h",
+        "bits.h",
+        "notification.h",
+        "threadpool.h",
+        "threadpool_interface.h",
+        "threadpool_options.h",
+    ],
+    visibility = ["//tensorflow/core:__pkg__"],
+)
+
 filegroup(
     name = "legacy_lib_core_all_headers",
     srcs = [
@@ -158,15 +187,6 @@ filegroup(
         "threadpool.h",
         "threadpool_interface.h",
         "threadpool_options.h",
-    ],
-    visibility = ["//tensorflow/core:__pkg__"],
-)
-
-filegroup(
-    name = "legacy_lib_core_all_srcs",
-    srcs = [
-        "arena.cc",
-        "bitmap.cc",
     ],
     visibility = ["//tensorflow/core:__pkg__"],
 )

tensorflow/core/lib/gtl/BUILD
@@ -189,6 +189,36 @@ filegroup(
     visibility = ["//tensorflow/core:__pkg__"],
 )
 
+# Export source files needed for mobile builds, which do not use granular targets.
+filegroup(
+    name = "mobile_srcs_no_runtime",
+    srcs = [
+        "array_slice.h",
+        "flatmap.h",
+        "flatrep.h",
+        "inlined_vector.h",
+    ],
+    visibility = ["//tensorflow/core:__pkg__"],
+)
+
+filegroup(
+    name = "mobile_srcs_only_runtime",
+    srcs = [
+        "cleanup.h",
+        "edit_distance.h",
+        "flatset.h",
+        "int_type.h",
+        "iterator_range.h",
+        "manual_constructor.h",
+        "map_util.h",
+        "optional.h",
+        "priority_queue_util.h",
+        "subtle/map_traits.h",
+        "top_n.h",
+    ],
+    visibility = ["//tensorflow/core:__pkg__"],
+)
+
 filegroup(
     name = "legacy_lib_gtl_all_headers",
     srcs = [

tensorflow/core/lib/hash/BUILD
@@ -49,20 +49,30 @@ cc_library(
     ],
 )
 
+# Export source files needed for mobile builds, which do not use granular targets.
 filegroup(
-    name = "legacy_lib_hash_all_headers",
+    name = "mobile_srcs_no_runtime",
     srcs = [
-        "crc32c.h",
         "hash.h",
     ],
     visibility = ["//tensorflow/core:__pkg__"],
 )
 
 filegroup(
-    name = "legacy_lib_hash_all_srcs",
+    name = "mobile_srcs_only_runtime",
     srcs = [
         "crc32c.cc",
+        "crc32c.h",
         "crc32c_accelerate.cc",
+    ],
+    visibility = ["//tensorflow/core:__pkg__"],
+)
+
+filegroup(
+    name = "legacy_lib_hash_all_headers",
+    srcs = [
+        "crc32c.h",
+        "hash.h",
     ],
     visibility = ["//tensorflow/core:__pkg__"],
 )

commit eabf051caaaff592623c2919a7c85436e459df56

For some inference models (maybe when there are 2 or more saved models in one global inference context) some TF libraries got loaded twice and some global variables got overwritten.
This bug was introduced in de37b1e - TF statically loads a bunch of TF_VARIANT decoders,
but then it dynamically loads the same (or some) libraries and overwrites global lists,
this bug has actually been there forever, but no one uses TF_VARIANT to store bool or int32,
so when those got overwritten (actually, lost), no one cared.

But variant decoder for TensorList got lost and reverting de37b1e should have fixed that.
This patch fixed the bug by explicitly calling single static function defined not in a header, but in C code.

Please see https://github.com/tensorflow/tensorflow/issues/44428 for how
this bug was found and which fix worked for macos, the second commit
will fix it for everyone.

tensorflow/core/framework/variant_op_registry.cc
@@ -52,6 +52,16 @@ std::unordered_set<string>* UnaryVariantOpRegistry::PersistentStringStorage() {
   return string_storage;
 }
 
+// Get a pointer to a global UnaryVariantOpRegistry object
+UnaryVariantOpRegistry* UnaryVariantOpRegistryGlobal() {
+  static UnaryVariantOpRegistry* global_unary_variant_op_registry = NULL;
+
+  if (global_unary_variant_op_registry == NULL) {
+    global_unary_variant_op_registry = new UnaryVariantOpRegistry;
+  }
+  return global_unary_variant_op_registry;
+}
+
 UnaryVariantOpRegistry::VariantDecodeFn* UnaryVariantOpRegistry::GetDecodeFn(
     StringPiece type_name) {
   auto found = decode_fns.find(type_name);

tensorflow/core/framework/variant_op_registry.h
@@ -60,6 +60,9 @@ enum VariantDeviceCopyDirection {
   DEVICE_TO_DEVICE = 3,
 };
 
+class UnaryVariantOpRegistry;
+extern UnaryVariantOpRegistry* UnaryVariantOpRegistryGlobal();
+
 class UnaryVariantOpRegistry {
  public:
   typedef std::function<bool(Variant*)> VariantDecodeFn;
@@ -174,9 +177,7 @@ class UnaryVariantOpRegistry {
 
   // Get a pointer to a global UnaryVariantOpRegistry object
   static UnaryVariantOpRegistry* Global() {
-    static UnaryVariantOpRegistry* global_unary_variant_op_registry =
-        new UnaryVariantOpRegistry;
-    return global_unary_variant_op_registry;
+    return UnaryVariantOpRegistryGlobal();
   }
 
   // Get a pointer to a global persistent string storage object.