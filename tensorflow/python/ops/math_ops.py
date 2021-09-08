se7enstar25/tensorflow

commit 292d3094313136b77bb5f444561bc3ffc529b246

Generate AddN instead of AccumulateN in Python.
PiperOrigin-RevId: 265584413

tensorflow/python/ops/math_ops.py
@@ -2954,13 +2954,7 @@ def accumulate_n(inputs, shape=None, tensor_dtype=None, name=None):
   Optionally, pass `shape` and `tensor_dtype` for shape and type checking,
   otherwise, these are inferred.
 
-  `accumulate_n` performs the same operation as `tf.math.add_n`, but
-  does not wait for all of its inputs to be ready before beginning to sum.
-  This approach can save memory if inputs are ready at different times, since
-  minimum temporary storage is proportional to the output size rather than the
-  inputs' size.
-
-  `accumulate_n` is differentiable (but wasn't previous to TensorFlow 1.7).
+  `accumulate_n` performs the same operation as `tf.math.add_n`.
 
   For example:
 
@@ -3020,14 +3014,7 @@ def accumulate_n(inputs, shape=None, tensor_dtype=None, name=None):
     return inputs[0]
   elif len(inputs) == 1 and name is not None:
     return array_ops.identity(inputs[0], name=name)
-  elif context.executing_eagerly():
-    # TemporaryVariable not currently supported in eager mode; fall back
-    # onto AddN for now.
-    # TODO(frreiss) remove this once the lifetime of eager variables gets
-    # addressed
-    return add_n(inputs, name=name)
-  else:
-    return gen_math_ops.accumulate_nv2(inputs, name=name, shape=shape)  # pylint: disable=protected-access
+  return add_n(inputs, name=name)
 
 
 @ops.RegisterGradient("AccumulateNV2")

tensorflow/python/distribute/cross_device_ops_test.py
@@ -267,7 +267,7 @@ class SingleWorkerCrossDeviceOpsTest(CrossDeviceOpsTestBase):
           combinations.NamedObject(
               "AccumulateNCrossDeviceOp",
               cross_device_ops_lib.ReductionToOneDevice(
-                  accumulation_fn=math_ops.accumulate_n)),
+                  accumulation_fn=math_ops.add_n)),
       ],
       devices=[
           ["/cpu:0"],

tensorflow/python/kernel_tests/BUILD
@@ -3725,40 +3725,6 @@ tf_py_test(
     ],
 )
 
-tf_py_test(
-    name = "accumulate_n_test",
-    size = "small",
-    srcs = ["accumulate_n_test.py"],
-    additional_deps = [
-        "//third_party/py/numpy",
-        "//tensorflow/python:client_testlib",
-        "//tensorflow/python:framework_for_generated_wrappers",
-        "//tensorflow/python:framework_test_lib",
-        "//tensorflow/python:gradients",
-        "//tensorflow/python:math_ops",
-        "//tensorflow/python:platform_test",
-        "//tensorflow/python:variables",
-    ],
-)
-
-tf_py_test(
-    name = "accumulate_n_eager_test",
-    size = "small",
-    srcs = ["accumulate_n_eager_test.py"],
-    additional_deps = [
-        "//third_party/py/numpy",
-        "//tensorflow/python:client_testlib",
-        "//tensorflow/python:framework_for_generated_wrappers",
-        "//tensorflow/python:framework_test_lib",
-        "//tensorflow/python:gradients",
-        "//tensorflow/python:math_ops",
-        "//tensorflow/python:resource_variable_ops",
-        "//tensorflow/python/eager:backprop",
-        "//tensorflow/python/eager:context",
-        "//tensorflow/python/eager:tape",
-    ],
-)
-
 # Custom op tests
 tf_custom_op_library(
     name = "ackermann_op.so",

tensorflow/python/kernel_tests/accumulate_n_eager_test.py
@@ -1,70 +1,0 @@
-# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-"""Tests for new version of accumulate_n op."""
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-import numpy as np
-
-from tensorflow.python.eager import backprop
-
-
-from tensorflow.python.framework import constant_op
-from tensorflow.python.framework import ops
-from tensorflow.python.framework import test_util
-from tensorflow.python.ops import math_ops
-from tensorflow.python.ops import resource_variable_ops
-from tensorflow.python.platform import test
-
-
-class AccumulateNV2EagerTest(test_util.TensorFlowTestCase):
-  """Tests of the new, differentiable version of accumulate_n."""
-
-  def testMinimalEagerMode(self):
-    forty = constant_op.constant(40)
-    two = constant_op.constant(2)
-    answer = math_ops.accumulate_n([forty, two])
-    self.assertEqual(42, answer.numpy())
-
-  def testFloat(self):
-    np.random.seed(12345)
-    x = [np.random.random((1, 2, 3, 4, 5)) - 0.5 for _ in range(5)]
-    tf_x = ops.convert_n_to_tensor(x)
-    self.assertAllClose(sum(x), math_ops.accumulate_n(tf_x))
-    self.assertAllClose(x[0] * 5,
-                        math_ops.accumulate_n([tf_x[0]] * 5))
-
-  def testGrad(self):
-    np.random.seed(42)
-    num_inputs = 3
-    input_vars = [
-        resource_variable_ops.ResourceVariable(10.0 * np.random.random(),
-                                               name="t%d" % i)
-        for i in range(0, num_inputs)
-    ]
-
-    def fn(first, second, third):
-      return math_ops.accumulate_n([first, second, third])
-
-    grad_fn = backprop.gradients_function(fn)
-    grad = grad_fn(input_vars[0], input_vars[1], input_vars[2])
-    self.assertAllEqual(np.repeat(1.0, num_inputs),  # d/dx (x + y + ...) = 1
-                        [elem.numpy() for elem in grad])
-
-
-if __name__ == "__main__":
-  ops.enable_eager_execution()
-  test.main()

tensorflow/python/kernel_tests/accumulate_n_test.py
@@ -1,160 +1,0 @@
-# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-"""Tests for new version of accumulate_n op."""
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-import numpy as np
-
-from tensorflow.python.framework import dtypes as dtypes_lib
-from tensorflow.python.framework import ops
-from tensorflow.python.framework import test_util
-from tensorflow.python.ops import array_ops
-from tensorflow.python.ops import gradients
-from tensorflow.python.ops import math_ops
-from tensorflow.python.ops import variables
-from tensorflow.python.ops.control_flow_ops import while_loop as while_loop_v1
-from tensorflow.python.platform import googletest
-
-
-class AccumulateNV2Test(test_util.TensorFlowTestCase):
-  """Tests of the new, differentiable version of accumulate_n."""
-
-  @test_util.run_deprecated_v1
-  def testFloat(self):
-    np.random.seed(12345)
-    x = [np.random.random((1, 2, 3, 4, 5)) - 0.5 for _ in range(5)]
-    tf_x = ops.convert_n_to_tensor(x)
-    with self.session(use_gpu=True):
-      self.assertAllClose(sum(x), math_ops.accumulate_n(tf_x).eval())
-      self.assertAllClose(x[0] * 5,
-                          math_ops.accumulate_n([tf_x[0]] * 5).eval())
-
-  @test_util.run_deprecated_v1
-  def testInt(self):
-    np.random.seed(54321)
-    x = [np.random.randint(-128, 128, (5, 4, 3, 2, 1)) for _ in range(6)]
-    tf_x = ops.convert_n_to_tensor(x)
-    with self.session(use_gpu=True):
-      self.assertAllEqual(sum(x), math_ops.accumulate_n(tf_x).eval())
-      self.assertAllEqual(x[0] * 6,
-                          math_ops.accumulate_n([tf_x[0]] * 6).eval())
-
-  @test_util.run_deprecated_v1
-  def testUnknownShape(self):
-    with self.session(use_gpu=True):
-      x0 = array_ops.placeholder(dtype=dtypes_lib.int32, shape=[None])
-      acc = math_ops.accumulate_n([x0, x0], shape=[None])
-      self.assertAllEqual([2, 4], acc.eval(feed_dict={x0: [1, 2]}))
-
-  @test_util.run_deprecated_v1
-  def testGrad(self):
-    np.random.seed(42)
-    for num_inputs in range(1, 10):
-      with self.cached_session(use_gpu=True) as sess:
-        input_vars = [
-            variables.Variable(10.0 * np.random.random())
-            for _ in range(0, num_inputs)
-        ]
-        accum_n = math_ops.accumulate_n(input_vars)
-        self.evaluate(variables.global_variables_initializer())
-        accum_n_grad = gradients.gradients(accum_n, input_vars)
-        self.assertAllEqual(
-            np.repeat(1.0, num_inputs),  # d/dx (x + y + ...) = 1
-            [g.eval() for g in accum_n_grad])
-
-  # The tests below used to be in a separate class under cwise_ops_test.py,
-  # which did not run in the default test target.
-  # Putting them here so that everything that exercises AccumulateNV2 is in
-  # one place and the default build runs all unit tests.
-  def testSimple(self):
-    with self.cached_session():
-      random_arrays = [
-          np.random.rand(16, 16, 16, 16).astype(np.float32) for _ in range(20)
-      ]
-      random_tensors = [
-          ops.convert_to_tensor(x, dtype=dtypes_lib.float32)
-          for x in random_arrays
-      ]
-      tf_val = math_ops.accumulate_n(random_tensors)
-      np_val = random_arrays[0]
-      for random_array in random_arrays[1:]:
-        np_val += random_array
-      self.assertAllClose(np_val, self.evaluate(tf_val))
-
-  # Test that AccumulateNV2 rewrite correctly add edges necessary to propagate
-  # while loop execution frame to all nodes.
-  def testAccumulateInsideWhileLoop(self):
-    with self.cached_session():
-      random_arrays = [
-          np.random.rand(16, 16, 16, 16).astype(np.float32) for _ in range(20)
-      ]
-      random_tensors = [
-          ops.convert_to_tensor(x, dtype=dtypes_lib.float32)
-          for x in random_arrays
-      ]
-
-      def cond_fn(i, acc, tensors):
-        del acc, tensors  # unused
-        return i < 1  # do just one iteration
-
-      def body_fn(i, acc, tensors):
-        return i + 1, acc + math_ops.accumulate_n(tensors), tensors
-
-      zeros = np.zeros((16, 16, 16, 16)).astype(np.float32)
-      _, tf_val, _ = while_loop_v1(cond_fn, body_fn, (0, zeros, random_tensors))
-      np_val = random_arrays[0]
-      for random_array in random_arrays[1:]:
-        np_val += random_array
-      self.assertAllClose(np_val, self.evaluate(tf_val))
-
-  def testZeroArgs(self):
-    with self.cached_session():
-      with self.assertRaises(ValueError):
-        tf_val = math_ops.accumulate_n([])
-        self.evaluate(tf_val)
-
-  def testWrongShape(self):
-    with self.cached_session():
-      with self.assertRaises(ValueError):
-        a = variables.Variable(0.2)
-        b = variables.Variable(0.1)
-        math_ops.accumulate_n([a, b], shape=[2, 2])  # Should be shape=[]
-
-  def testIncompatibleShapes(self):
-    with self.cached_session():
-      with self.assertRaises(ValueError):
-        a = variables.Variable(np.array([0.1, 0.2]))
-        b = variables.Variable(np.array([[0.3], [0.4]]))
-        math_ops.accumulate_n([a, b])
-
-  def testWrongType(self):
-    with self.cached_session():
-      with self.assertRaises(TypeError):
-        a = variables.Variable(0.2, dtype=np.float32)
-        b = variables.Variable(0.1, dtype=np.float32)
-        math_ops.accumulate_n([a, b], tensor_dtype=np.int32)
-
-  def testWrongTypeOneInput(self):
-    # Scenario that used to trigger a bug, even when testWrongType() worked
-    with self.cached_session():
-      with self.assertRaises(TypeError):
-        a = variables.Variable(0.2, dtype=np.float32)
-        math_ops.accumulate_n([a], tensor_dtype=np.int32)
-
-
-if __name__ == "__main__":
-  googletest.main()

tensorflow/python/kernel_tests/cwise_ops_test.py
@@ -32,7 +32,6 @@ from tensorflow.python.ops import gradient_checker
 from tensorflow.python.ops import gradients_impl
 from tensorflow.python.ops import math_ops
 from tensorflow.python.ops import nn_grad  # pylint: disable=unused-import
-from tensorflow.python.ops import variables
 from tensorflow.python.platform import test
 
 _ADD = lambda x, y: x + y
@@ -1185,51 +1184,6 @@ class ComplexMakeRealImagTest(test.TestCase):
   def testMulGradient(self):
     data = np.arange(1, 2, 0.125).reshape([2, 4]).astype(np.float32)
     self._compareMulGradient(data)
-
-
-class AccumulateTest(test.TestCase):
-
-  def testSimple(self):
-    with self.cached_session():
-      random_arrays = [
-          np.random.rand(16, 16, 16, 16).astype(np.float32) for _ in range(20)
-      ]
-      random_tensors = [
-          ops.convert_to_tensor(x, dtype=dtypes_lib.float32)
-          for x in random_arrays
-      ]
-      tf_val = math_ops.accumulate_n(random_tensors)
-      np_val = random_arrays[0]
-      for random_array in random_arrays[1:]:
-        np_val += random_array
-      self.assertAllClose(np_val, self.evaluate(tf_val))
-
-  def testZeroArgs(self):
-    with self.cached_session():
-      with self.assertRaises(ValueError):
-        tf_val = math_ops.accumulate_n([])
-        self.evaluate(tf_val)
-
-  def testWrongShape(self):
-    with self.cached_session():
-      with self.assertRaises(ValueError):
-        a = variables.Variable(0.2)
-        b = variables.Variable(0.1)
-        math_ops.accumulate_n([a, b], shape=[2, 2])  # Should be shape=[]
-
-  def testWrongType(self):
-    with self.cached_session():
-      with self.assertRaises(TypeError):
-        a = variables.Variable(0.2, dtype=np.float32)
-        b = variables.Variable(0.1, dtype=np.float32)
-        math_ops.accumulate_n([a, b], tensor_dtype=np.int32)
-
-  def testWrongTypeOneInput(self):
-    # Scenario that used to trigger a bug, even when testWrongType() worked
-    with self.cached_session():
-      with self.assertRaises(TypeError):
-        a = variables.Variable(0.2, dtype=np.float32)
-        math_ops.accumulate_n([a], tensor_dtype=np.int32)
 
 
 class PolyvalTest(test.TestCase):

tensorflow/python/ops/gradients_util.py
@@ -902,19 +902,12 @@ class AggregationMethod(object):
     performance, but it can improve memory utilization because the
     gradients can be released earlier.
 
-  * `EXPERIMENTAL_ACCUMULATE_N`: Gradient terms are summed using the
-    "AccumulateN" op (see `tf.accumulate_n`), which accumulates the
-    overall sum in a single buffer that is shared across threads.
-    This method of summing gradients can result in a lower memory footprint
-    and lower latency at the expense of higher CPU/GPU utilization.
-    For gradients of types that "AccumulateN" does not support, this
-    summation method falls back on the behavior of `EXPERIMENTAL_TREE`
   """
   ADD_N = 0
   DEFAULT = ADD_N
   # The following are experimental and may not be supported in future releases.
   EXPERIMENTAL_TREE = 1
-  EXPERIMENTAL_ACCUMULATE_N = 2
+  EXPERIMENTAL_ACCUMULATE_N = 2  # An alias for EXPERIMENTAL_ADD_N = 1
 
 
 def _AggregatedGrads(grads,
@@ -974,19 +967,7 @@ def _AggregatedGrads(grads,
         out_grads[i] = out_grad[0]
       elif all(isinstance(g, ops.Tensor) for g in out_grad if g is not None):
         tensor_shape = _AccumulatorShape(out_grad)
-        if (aggregation_method == AggregationMethod.EXPERIMENTAL_ACCUMULATE_N
-            and len(out_grad) > 2 and tensor_shape.is_fully_defined()):
-          # The benefit of using AccumulateN is that its inputs can be combined
-          # in any order and this can allow the expression to be evaluated with
-          # a smaller memory footprint.  When used with gpu_allocator_retry,
-          # it is possible to compute a sum of terms which are much larger than
-          # total GPU memory.
-          # AccumulateN can currently only be used if we know the shape for
-          # an accumulator variable.  If this is not known, or if we only have
-          # 2 grads then we fall through to the "tree" case below.
-          used = "accumulate_n"
-          out_grads[i] = math_ops.accumulate_n(out_grad)
-        elif aggregation_method in [
+        if aggregation_method in [
             AggregationMethod.EXPERIMENTAL_TREE,
             AggregationMethod.EXPERIMENTAL_ACCUMULATE_N
         ]:

tensorflow/python/ops/math_ops_test.py
@@ -326,27 +326,6 @@ class ScalarMulTest(test_util.TensorFlowTestCase):
       self.assertAllEqual(self.evaluate(x.indices), [0, 2, 5])
 
 
-class AccumulateNTest(test_util.TensorFlowTestCase):
-
-  @test_util.run_deprecated_v1
-  def testFloat(self):
-    np.random.seed(12345)
-    x = [np.random.random((1, 2, 3, 4, 5)) - 0.5 for _ in range(5)]
-    tf_x = ops.convert_n_to_tensor(x)
-    with self.session(use_gpu=True):
-      self.assertAllClose(sum(x), math_ops.accumulate_n(tf_x).eval())
-      self.assertAllClose(x[0] * 5, math_ops.accumulate_n([tf_x[0]] * 5).eval())
-
-  @test_util.run_deprecated_v1
-  def testInt(self):
-    np.random.seed(54321)
-    x = [np.random.randint(-128, 128, (5, 4, 3, 2, 1)) for _ in range(6)]
-    tf_x = ops.convert_n_to_tensor(x)
-    with self.session(use_gpu=True):
-      self.assertAllEqual(sum(x), math_ops.accumulate_n(tf_x).eval())
-      self.assertAllEqual(x[0] * 6, math_ops.accumulate_n([tf_x[0]] * 6).eval())
-
-
 class AddNTest(test_util.TensorFlowTestCase):
 
   @test_util.run_deprecated_v1

commit c5f293cd8350fb443184d01d86a2223ba8dfa298

Remove incorrect documentation from tf.math.add_n.
The documentation previously incorrectly implied accumulate_n did not wait for all its inputs to be ready. The commit https://github.com/tensorflow/tensorflow/commit/292d3094313136b77bb5f444561bc3ffc529b246 caused accumulate_n to do the same thing as add_n.

PiperOrigin-RevId: 395531673
Change-Id: I48dddc89f1cafd9ff0c41fb2f0d1d4a507b55e6e

tensorflow/python/ops/math_ops.py
@@ -3987,11 +3987,7 @@ def add(x, y, name=None):
 def add_n(inputs, name=None):
   """Adds all input tensors element-wise.
 
-  `tf.math.add_n` performs the same operation as `tf.math.accumulate_n`, but it
-  waits for all of its inputs to be ready before beginning to sum.
-  This buffering can result in higher memory consumption when inputs are ready
-  at different times, since the minimum temporary storage required is
-  proportional to the input size rather than the output size.
+  `tf.math.add_n` performs the same operation as `tf.math.accumulate_n`.
 
   This op does not [broadcast](
   https://docs.scipy.org/doc/numpy-1.13.0/user/basics.broadcasting.html)