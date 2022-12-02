se7enstar25/tensorflow

commit f511aa077646c1028c819090064945e6f109ff94

Make coreml Add and Mul op builders support fp16 inputs. Convert fp16 input data to fp32 before passing it to coreml delegate.
PiperOrigin-RevId: 491780159

tensorflow/lite/delegates/coreml/coreml_delegate_kernel.mm
@@ -14,6 +14,7 @@ limitations under the License.
 ==============================================================================*/
 #include "tensorflow/lite/delegates/coreml/coreml_delegate_kernel.h"
 
+#include "third_party/FP16/include/fp16.h"
 #include "tensorflow/lite/context_util.h"
 #include "tensorflow/lite/kernels/internal/optimized/optimized_ops.h"
 #include "tensorflow/lite/kernels/internal/types.h"
@@ -210,9 +211,18 @@ TfLiteStatus CoreMlDelegateKernel::Invoke(TfLiteContext* context, TfLiteNode* no
       for (int i = 0; i < input_tensor_ids_.size(); ++i) {
         const int tensor_id = input_tensor_ids_[i];
         TfLiteTensor* tensor = &context->tensors[tensor_id];
+        const float* float_ptr = tensor->data.f;
+        if (tensor->type == kTfLiteFloat16) {
+          std::vector<float> floats = std::vector<float>(NumElements(tensor));
+          const uint16_t* float16_ptr = reinterpret_cast<uint16_t const*>(tensor->data.f16);
+          for (int j = 0; j < floats.size(); j++) {
+            floats[j] = fp16_ieee_to_fp32_value(float16_ptr[j]);
+          }
+          float_ptr = floats.data();
+        }
         // Transpose input to CHW.
         // TODO(b/143992544): try adding transpose op for inputs.
-        TransposeToCHW(tensor->data.f, inputs_[i].data.data(), tensor->dims);
+        TransposeToCHW(float_ptr, inputs_[i].data.data(), tensor->dims);
       }
 
       if (![executor_ invokeWithInputs:inputs_ outputs:outputs_]) {

tensorflow/lite/delegates/coreml/BUILD
@@ -85,5 +85,6 @@ objc_library(
         "//tensorflow/lite/kernels:kernel_util",
         "//tensorflow/lite/kernels/internal:optimized_base",
         "//tensorflow/lite/kernels/internal:types",
+        "@FP16",
     ],
 )

tensorflow/lite/delegates/coreml/builders/add_op_builder.cc
@@ -21,6 +21,7 @@ limitations under the License.
 #include "tensorflow/lite/core/c/builtin_op_data.h"
 #include "tensorflow/lite/delegates/coreml/builders/activation_layer_builder.h"
 #include "tensorflow/lite/delegates/coreml/builders/op_factory.h"
+#include "tensorflow/lite/delegates/coreml/builders/util.h"
 #include "tensorflow/lite/kernels/internal/tensor_ctypes.h"
 #include "tensorflow/lite/kernels/kernel_util.h"
 
@@ -75,10 +76,10 @@ TfLiteStatus AddOpBuilder::RegisterInputs(const TfLiteIntArray* inputs,
   // store constant, scalar value into MultiplyLayerParams directly.
   if (IsConstantTensor(input_0) && NumElements(input_0) == 1) {
     AddInput(inputs->data[1]);
-    SetAlpha(GetTensorData<float>(input_0)[0]);
+    SetAlpha(GetScalarFloatFromTensor(input_0));
   } else if (IsConstantTensor(input_1) && NumElements(input_1) == 1) {
     AddInput(inputs->data[0]);
-    SetAlpha(GetTensorData<float>(input_1)[0]);
+    SetAlpha(GetScalarFloatFromTensor(input_1));
   } else {
     AddInput(inputs->data[0]);
     AddInput(inputs->data[1]);

tensorflow/lite/delegates/coreml/builders/mul_op_builder.cc
@@ -81,10 +81,10 @@ TfLiteStatus MulOpBuilder::RegisterInputs(const TfLiteIntArray* inputs,
   // store constant, scalar value into MultiplyLayerParams directly.
   if (IsConstantTensor(input_0) && NumElements(input_0) == 1) {
     AddInput(inputs->data[1]);
-    SetAlpha(GetTensorData<float>(input_0)[0]);
+    SetAlpha(GetScalarFloatFromTensor(input_0));
   } else if (IsConstantTensor(input_1) && NumElements(input_1) == 1) {
     AddInput(inputs->data[0]);
-    SetAlpha(GetTensorData<float>(input_1)[0]);
+    SetAlpha(GetScalarFloatFromTensor(input_1));
   } else {
     AddInput(inputs->data[0]);
     AddInput(inputs->data[1]);

tensorflow/lite/delegates/coreml/builders/util.cc
@@ -16,7 +16,10 @@ limitations under the License.
 
 #include <vector>
 
+#include "fp16.h"  // from @FP16
+#include "tensorflow/lite/core/c/common.h"
 #include "tensorflow/lite/delegates/coreml/builders/op_validator.h"
+#include "tensorflow/lite/kernels/internal/tensor_ctypes.h"
 #include "tensorflow/lite/kernels/kernel_util.h"
 
 namespace tflite {
@@ -76,10 +79,26 @@ bool IsBroadcastable(const TfLiteTensor* input_0, const TfLiteTensor* input_1) {
   return true;
 }
 
+bool IsBinaryOpSupportedType(const TfLiteTensor* tensor) {
+  return tensor->type == kTfLiteFloat32 ||
+         (tensor->type == kTfLiteFloat16 && IsConstantTensor(tensor));
+}
+
 bool IsBinaryOpSupported(const TfLiteRegistration* registration,
                          const TfLiteNode* node, TfLiteContext* context) {
-  return IsBroadcastable(GetInput(context, node, 0),
-                         GetInput(context, node, 1));
+  const auto* input_0 = GetInput(context, node, 0);
+  const auto* input_1 = GetInput(context, node, 1);
+  if (IsBinaryOpSupportedType(input_0) && IsBinaryOpSupportedType(input_1)) {
+    return IsBroadcastable(input_0, input_1);
+  }
+  return false;
+}
+
+float GetScalarFloatFromTensor(const TfLiteTensor* tensor) {
+  if (tensor->type == kTfLiteFloat16) {
+    return fp16_ieee_to_fp32_value(GetTensorData<uint16_t>(tensor)[0]);
+  }
+  return GetTensorData<float>(tensor)[0];
 }
 
 }  // namespace coreml

tensorflow/lite/delegates/coreml/builders/util.h
@@ -30,6 +30,10 @@ namespace coreml {
 bool IsBinaryOpSupported(const TfLiteRegistration* registration,
                          const TfLiteNode* node, TfLiteContext* context);
 
+// Gets the float scalar value from the given tensor. The tensor should be a
+// constant float32/float16 tensor of size 1.
+float GetScalarFloatFromTensor(const TfLiteTensor* tensor);
+
 }  // namespace coreml
 }  // namespace delegates
 }  // namespace tflite

tensorflow/lite/delegates/coreml/builders/util_test.cc
@@ -18,6 +18,7 @@ limitations under the License.
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include "third_party/eigen3/Eigen/Core"
 #include "tensorflow/lite/c/common.h"
 
 using tflite::delegates::coreml::IsBinaryOpSupported;
@@ -37,6 +38,7 @@ class IsBinaryOpSupportedTest : public testing::Test {
 
     for (auto& tensor : tensors_) {
       tensor.allocation_type = kTfLiteArenaRw;
+      tensor.type = kTfLiteFloat32;
       tensor.dims = nullptr;
     }
   }
@@ -107,4 +109,34 @@ TEST_F(IsBinaryOpSupportedTest, NotSupportedBroadcastTest) {
     FreeInputShapes();
   }
 }
+
+TEST_F(IsBinaryOpSupportedTest, SupportConstFloat16InputTest) {
+  tensors_[0].allocation_type = kTfLiteMmapRo;
+  tensors_[0].type = kTfLiteFloat16;
+  SetInputShapes({{2, 2, 3}, {2, 2, 3}});
+  ASSERT_TRUE(IsBinaryOpSupported(nullptr, &node_, &context_));
+}
+
+TEST_F(IsBinaryOpSupportedTest, NotSupportNonConstFloat16InputTest) {
+  tensors_[0].allocation_type = kTfLiteArenaRw;
+  tensors_[0].type = kTfLiteFloat16;
+  SetInputShapes({{2, 2, 3}, {2, 2, 3}});
+  ASSERT_FALSE(IsBinaryOpSupported(nullptr, &node_, &context_));
+}
+
+TEST(UtilTest, GetScalarFloatFromTensorTest) {
+  std::vector<Eigen::half> half{Eigen::half{-535.54f}};
+  std::vector<float> float32{-535.54f};
+  TfLiteTensor tensor;
+  tensor.type = kTfLiteFloat16;
+  tensor.data.data = reinterpret_cast<uint16_t*>(half.data());
+  EXPECT_THAT(tflite::delegates::coreml::GetScalarFloatFromTensor(&tensor),
+              testing::FloatNear(-535.54f, 0.1f));
+
+  tensor.type = kTfLiteFloat32;
+  tensor.data.data = float32.data();
+  EXPECT_THAT(tflite::delegates::coreml::GetScalarFloatFromTensor(&tensor),
+              testing::FloatNear(-535.54f, 0.f));
+}
+
 }  // namespace

commit cd5a194faba0033cb87258200147bdfd0f376fa6

Fixed ios build issues caused by commit https://github.com/tensorflow/tensorflow/commit/f511aa077646c1028c819090064945e6f109ff94. Add required dependencies to coreml/builders/BUILD.oss. Also update copybara to transform includes in objc++ files.
PiperOrigin-RevId: 492347703

tensorflow/lite/delegates/coreml/builders/BUILD
@@ -69,6 +69,8 @@ cc_library(
         "//tensorflow/lite/c:common",
         "//tensorflow/lite/core/c:common",
         "//tensorflow/lite/kernels:kernel_util",
+        "//tensorflow/lite/kernels/internal:tensor",
+        "//third_party/FP16",
     ],
 )
 
@@ -79,6 +81,7 @@ cc_test(
         ":util",
         "//tensorflow/lite/c:common",
         "//tensorflow/lite/core/c:common",
+        "//third_party/eigen3",
         "@com_google_googletest//:gtest_main",
     ],
 )

tensorflow/lite/delegates/coreml/coreml_delegate_kernel.mm
@@ -14,7 +14,7 @@ limitations under the License.
 ==============================================================================*/
 #include "tensorflow/lite/delegates/coreml/coreml_delegate_kernel.h"
 
-#include "third_party/FP16/include/fp16.h"
+#include "fp16.h"  // from @FP16
 #include "tensorflow/lite/context_util.h"
 #include "tensorflow/lite/kernels/internal/optimized/optimized_ops.h"
 #include "tensorflow/lite/kernels/internal/types.h"